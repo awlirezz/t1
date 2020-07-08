9b9769556600acb5320001adb48f76d542130bc3
awlirezz <p2tofighi@gmail.com>
2020-07-08 17:31:05 +0430

    date label added

---
+++ awli.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+
+def say():
+    d =datetime.now()
+    a.config(text=)
+
+root = tk.Tk()
+a=tk.Label(root, text ='')
+a.pack()
+b=tk.Button(root,text = 'Get turn!',command=say)
+b.pack()
+
+
+
+
+root.mainloop()
