abed1b1444c976084e2fb6081c6a64ce0d33efcf
amir102800 <amirmoahammadalizadeh1111@gmail.com>
2020-07-29 17:28:43 +0430

    chess timer

--- awli2.py
+++ awli2.py
@@ -1,12 +1,29 @@
 import tkinter as tk
+from time import sleep
+
+def stop(side):
+    global flag
+    if side == 'left':
+        flag = True
+    else:
+        flag = False
 
 
-def stop():
+def start():
     pass
-
+    while True:
+        sleep(1)
+        if flag:
+            timer['right'] -= 1
+        else:
+            timer['left'] -= 1
+     print(timer)
 
 root = tk.Tk()
-
+timer = {'left': 1200
+    ,
+         'right': 1200}
+flag = True
 tk.Label(root, text='Left Player').grid(row=0, column=0)
 
 tk.Label(root, text='Right Player').grid(row=0, column=3)
@@ -26,6 +43,6 @@ tk.Button(root, text='Stop', command=stop).grid(row=2, column=3)
 
 tk.Button(root, text='Start').grid(row=3, column=2)
 
-tk.Button(root, text='Cancel').grid(row=4, column=2)
+tk.Button(root, text='Cancel', command=root.destroy).grid(row=4, column=2)
 
 root.mainloop()
