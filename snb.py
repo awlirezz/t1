70de3a6e14c56a9d84f461b747e94b30476af608
Mohammad-Mahdi-kaseb <mahdi15.20781338@gmail.com>
2020-09-02 17:32:50 +0430

    snb

--- snb.py
+++ snb.py
@@ -10,6 +10,25 @@ def start():
 def callback1(arg1, arg2, arg3):
     p1.set(n_p_1.get())
 
+def callback2(arg1, arg2, arg3):
+    p2.set(n_p_2.get())
+
+def callback3(arg1, arg2, arg3):
+    p3.set(n_p_3.get())
+
+def callback_t_1(arg1, arg2, arg3):
+    t1.set('%02d:%02d:%02d'%(int(h_p_1.get()),int(m_p_1.get()),int(s_p_1.get())))
+
+
+def callback_t_2(arg1, arg2, arg3):
+    t2.set('%02d:%02d:%02d'%(int(h_p_2.get()),int(m_p_2.get()),int(s_p_2.get())))
+
+
+
+def callback_t_3(arg1, arg2, arg3):
+    t3.set('%02d:%02d:%02d'%(int(h_p_3.get()),int(m_p_3.get()),int(s_p_3.get())))
+
+
 
 root = tk.Tk()
 
@@ -87,4 +106,93 @@ tk.Spinbox(f1,
            textvariable=s_p_1,
            width=2,
            state="readonly").grid(row=0, column=2)
+# ############ Patient 2 Name Timer ############ #
+patient2 = tk.LabelFrame(patient, text='Patient2')
+patient2.grid(row=1, column=0, padx=10)
+
+tk.Label(patient2, text='Name').grid(row=0, column=0)
+tk.Label(patient2, text='Timer').grid(row=1, column=0)
+n_p_2 = tk.StringVar()
+n_p_2.trace('w', callback2)
+tk.Entry(patient2, textvariable=n_p_2).grid(row=0, column=1)
+# ------------------Timer-------------------- #
+h_p_2 = tk.StringVar()
+h_p_2.set('12')
+m_p_2 = tk.StringVar()
+m_p_2.set('30')
+s_p_2 = tk.StringVar()
+s_p_2.set('30')
+f2 = tk.Frame(patient2)
+f2.grid(row=1, column=1)
+tk.Spinbox(f2,
+           from_=0,
+           to=23,
+           wrap=True,
+           textvariable=h_p_2,
+           width=2,
+           state="readonly").grid(row=0, column=0)
+tk.Spinbox(f2,
+           from_=0,
+           to=59,
+           wrap=True,
+           textvariable=m_p_2,
+           width=2,
+           state="readonly").grid(row=0, column=1)
+tk.Spinbox(f2,
+           from_=0,
+           to=59,
+           wrap=True,
+           textvariable=s_p_2,
+           width=2,
+           state="readonly").grid(row=0, column=2)
+
+# ############ Patient  3 Name Timer ############ #
+patient3 = tk.LabelFrame(patient, text='Patient3')
+patient3.grid(row=2, column=0, padx=10)
+
+tk.Label(patient3, text='Name').grid(row=0, column=0)
+tk.Label(patient3, text='Timer').grid(row=1, column=0)
+n_p_3 = tk.StringVar()
+n_p_3.trace('w', callback3)
+tk.Entry(patient3, textvariable=n_p_3).grid(row=0, column=1)
+# ------------------Timer-------------------- #
+h_p_3 = tk.StringVar()
+h_p_3.set('12')
+m_p_3 = tk.StringVar()
+m_p_3.set('30')
+s_p_3 = tk.StringVar()
+s_p_3.set('30')
+f3 = tk.Frame(patient3)
+f3.grid(row=1, column=1)
+tk.Spinbox(f3,
+           from_=0,
+           to=23,
+           wrap=True,
+           textvariable=h_p_3,
+           width=2,
+           state="readonly").grid(row=0, column=0)
+tk.Spinbox(f3,
+           from_=0,
+           to=59,
+           wrap=True,
+           textvariable=m_p_3,
+           width=2,
+           state="readonly").grid(row=0, column=1)
+tk.Spinbox(f3,
+           from_=0,
+           to=59,
+           wrap=True,
+           textvariable=s_p_3,
+           width=2,
+           state="readonly").grid(row=0, column=2)
+# ############ Timer Callbacks ################# #
+h_p_1.trace('w',callback_t_1)
+n_p_1.trace('w',callback_t_1)
+s_p_1.trace('w',callback_t_1)
+h_p_2.trace('w',callback_t_2)
+n_p_2.trace('w',callback_t_2)
+s_p_2.trace('w',callback_t_2)
+h_p_3.trace('w',callback_t_3)
+n_p_3.trace('w',callback_t_3)
+s_p_3.trace('w',callback_t_3)
 root.mainloop()
