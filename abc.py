179cd1763955eb2c81be95e09ed4708d991c40e9
Romina Shabanzadeh <Rominash@gmail.com>
2020-09-16 17:25:04 +0430

    wtf

---
+++ abc.py
@@ -0,0 +1,48 @@
+import tkinter as tk
+from tkinter import Frame
+import tkinter.ttk as ttk
+
+
+from abcl import *
+
+
+
+asd={'bg' : '#ff6d00'}
+asd1={'bg':'#64dd17'}
+root = tk.Tk()
+note = ttk.Notebook()
+note.grid(row=0, column=0)
+
+food=tk.Frame()
+drink=tk.Frame()
+receipt=tk.Frame()
+
+
+
+note.add(food, text='Food')
+note.add(drink, text='Drink')
+note.add(receipt, text='Receipt')
+
+
+f1 = tk.Frame(food,cnf=Label_cnf)
+f1.grid(row=0, column=0)
+
+
+# d1 = tk.Frame(drink)
+# d1.grid(row=0, column=0)
+
+
+# r1 = tk.Frame(receipt)
+# r1.grid(row=0, column=0)
+
+
+
+
+tk.Label(f1,text='Joje gang',cnf=asd).grid(row=0,column=0)
+
+tk.Label(f1,text='    *****     ',cnf=asd1).grid(row=1,column=0)
+
+tk.Label(f1,text='    2.5$      ',cnf=asd1).grid(row=2,column=0)
+
+
+root.mainloop()
