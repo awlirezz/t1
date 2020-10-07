0f2be4e78b01c74ea23272c59e1d60dd15d06675
awlirezz <68011627+awlirezz@users.noreply.github.com>
2020-10-07 14:54:27 +0330

    hmm

--- realrestuarant/config.py
+++ realrestuarant/config.py
@@ -2,3 +2,7 @@ label_cnf = {
     'bg': '#ffc107',
     'font': ('fixedsys', 20)
 }
+label_abc = {
+    'bg': '#FAFAFA',
+    'font': ('fixedsys', 20)
+}
--- realrestuarant/rst.py
+++ realrestuarant/rst.py
@@ -6,13 +6,13 @@ from config import *
 
 # $$$$$$$$$$$$$$$$$$$ Food Information $$$$$$$$$$$$$$$$$$$ #
 i = {
-    0: {'name': 'BaqaliQatoq',
+    0: {'name': '★BaqaliQatoq',
           'rating': 5,
           'review': 47,
           'price': 1.5,
           'des':'This is Iranian Food, which most used in north of IRAN Elit reprehen derit exce pteur dolor labore ipsum veniam exercitation deserunt.',
           'img': 'img\\cart.gif'},
-    1: {'name': 'SabziQormeh',
+    1: {'name': '★SabziQormeh',
           'rating': 4,
           'review': 72,
           'price': 1,
@@ -21,18 +21,21 @@ i = {
 }
 
 d = {
-    0: {'name': 'BaqaliQatoq',
-          'rating': 5,
+    0: {'name': '★Coca',
+          'rating': 4,
           'review': 47,
           'price': 1.5,
-          'des':'This is Iranian Food, which most used in north of IRAN Elit reprehen derit exce pteur dolor labore ipsum veniam exercitation deserunt.',
-          'img': 'img\\cart.gif'},
-    1: {'name': 'SabziQormeh',
+          },
+    1: {'name': '★Pepci',
+          'rating': 5,
+          'review': 72,
+          'price': 2.5 ,
+          },
+    2: {'name': '★Sprite',
           'rating': 4,
           'review': 72,
-          'price': 1,
-          'des':'This is Iranian Food, which most used in north of IRAN Elit reprehen derit exce pteur dolor labore ipsum veniam exercitation deserunt.',
-          'img': 'img\\cart.gif'}
+          'price': 0.5 ,
+          }  
 }
 
 image = {}
@@ -42,6 +45,8 @@ img = {}
 root = tk.Tk()
 root.title('Restauran Menu System')
 note = ttk.Notebook()
+
+
 note.grid(row=0 , column=0)
 # ################ NoteBook Tabs ############### #
 food = tk.Frame()
@@ -62,7 +67,8 @@ for j in range(len(i)):
         text=name,
         width=13,
         cnf=label_cnf,
-        anchor='w').grid(row=0, column=0, sticky=tk.W)
+
+        anchor='w').grid(row=0, column=0,sticky=tk.W)
 
     rating = i[j]['rating'] * '★' + '(' + str(i[j]['review']) +')'
     tk.Label(f1,
@@ -102,35 +108,33 @@ for j in range(len(i)):
 
 for a in range(len(d)):
     f1 = tk.Frame(drink,bg='#ffc107')
-    f1.grid(row=j, column=0)
+    f1.grid(row=a, column=0)
 
     name = d[a]['name']
     tk.Label(f1,
         text=name,
-        width=13,
+        width=15,
         cnf=label_cnf,
-        anchor='w').grid(row=0, column=0, sticky=tk.W)
+        anchor='w').grid(row=0, column=0,pady=4)
 
     rating = d[a]['rating'] * '★' + '(' + str(d[a]['review']) +')'
     tk.Label(f1,
         text=rating,
-        font='fixedsys',
-        anchor='nw').grid(row=1, column=0, sticky="nw", padx=10, pady=5)
+        font='fixedsys',cnf=label_abc,
+        ).grid(row=1, column=0, sticky="nw",padx=10)
 
     f1_5 = tk.Frame(f1, bg='#ffc107')
-    f1_5.grid(row=1, column=1)
+    f1_5.grid(row=a, column=0, sticky=tk.W)
 
-    image[a] = PhotoImage(file='img\\cart.gif').subsample(7)
-    tk.Label(f1_5, image=image[a], bg='#ffc107', fg='#ffffff').grid(row=0, column=0)
-    tk.Label(f1_5, text=2, font=('times', 15), bg='#ffc107').grid(row=0, column=1, sticky=tk.S)
-    tk.Button(f1_5, text='+').grid(row=0, column=2)
-    tk.Button(f1_5, text='-').grid(row=0, column=3)
+
+
+
 
 
     price = str(d[a]['price']) + '$' 
     tk.Label(f1,
-        text=price,
-        font='fixedsys').grid(row=0, column=1)
+        text=price,cnf=label_abc,
+        font='fixedsys').grid(row=0, column=2)
 
 
 
