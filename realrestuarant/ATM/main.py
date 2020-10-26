54e79b3bee46f8915c05932fba748e3fab631caf
PoulStar Student <pstudent@localhost.localdomain>
2020-10-26 17:24:47 +0330

    hm

--- realrestuarant/ATM/main.py
+++ realrestuarant/ATM/main.py
@@ -1,5 +1,47 @@
 import tkinter as tk
 import tkinter.ttk as ttk
+import hashlib
+
+
+def to_sha1(password):
+    return hashlib.sha1(password.encode('utf-8')).hexdigest()
+
+
+
+
+def register():
+    input_user = form_user.get()
+    input_pass = to_sha1(form_pass.get())           
+    form_user.set('')
+    form_pass.set('')           
+    print(input_pass,input_user)         
+    with open ('users.txt','a')as file:
+        file.write(f'user:{input_user} pass:{input_pass}\n')
+
+def login():
+    pass    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -27,9 +69,22 @@ tk.Label(register_form,text= 'Username').grid(row=0,column=0)
 tk.Label(register_form,text= 'Password:').grid(row=1,column=0)
 form_user=tk.StringVar()
 form_pass=tk.StringVar()
-tk.Entry(register_form,textvariable= 'form_user').grid(row=0,column=1)
-tk.Entry(register_form,textvariable= 'form_pass').grid(row=1,column=1)
-tk.Button(register_form,text='Register').grid(row=2,column=0,columnspan=2,sticky=tk.W+tk.E)
+tk.Entry(register_form,textvariable= form_user).grid(row=0,column=1)
+tk.Entry(register_form,textvariable= form_pass,show='*').grid(row=1,column=1)
+tk.Button(register_form,text='Register',command=register ).grid(row=2,column=0,columnspan=2,sticky=tk.W+tk.E)
+############################################################################################
+tk.Label(login_form,text= 'Username').grid(row=0,column=0)
+tk.Label(login_form,text= 'Password:').grid(row=1,column=0)
+login_user=tk.StringVar()
+login_pass=tk.StringVar()
+tk.Entry(login_form,textvariable=login_user).grid(row=0,column=1)
+tk.Entry(login_form,textvariable=login_pass,show='*').grid(row=1,column=1)
+tk.Button(login_form,text='login',command=login).grid(row=2,column=0,columnspan=2,sticky=tk.W+tk.E)
+
+
+
+
+
 
 
 
