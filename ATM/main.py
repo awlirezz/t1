0f5ba9113023b731853c9dc611adb817197f7463
amir102800 <amirmohammadalizadeh1111@gmail.com>
2020-11-02 17:27:56 +0330

    hm

--- ATM/main.py
+++ ATM/main.py
@@ -1,6 +1,18 @@
 import tkinter as tk
 import tkinter.ttk as ttk
 import hashlib
+import json
+
+
+def read_json(address):
+    with open(address)as file:
+        return json.load(file)
+
+
+def write_json(address,data):
+    with open(address,'w',encoding= 'utf-8')as file:
+        json.dump(data,file,ensure_ascii=False, indent=4)    
+
 
 
 def to_sha1(password):
@@ -13,33 +25,15 @@ def register():
     input_user = form_user.get()
     input_pass = to_sha1(form_pass.get())           
     form_user.set('')
-    form_pass.set('')           
-    print(input_pass,input_user)         
-    with open ('users.txt','a')as file:
-        file.write(f'user:{input_user} pass:{input_pass}\n')
+    form_pass.set('')  
+    file = read_json('names.json')
+    data = {'usernames':input_user,'password':input_pass}
+    file.append(data) 
+    write_json('names.json',file) 
+
 
 def login():
     pass    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -49,8 +43,7 @@ root = tk.Tk()
 root.title('Bank')
 
 
-#top= tk.Toplevel()
-#top.title('Form')
+
 
 note =ttk.Notebook()
 
@@ -67,23 +60,24 @@ note.grid(row=0,column=0)
 
 tk.Label(register_form,text= 'Username').grid(row=0,column=0)
 tk.Label(register_form,text= 'Password:').grid(row=1,column=0)
+
 form_user=tk.StringVar()
 form_pass=tk.StringVar()
 tk.Entry(register_form,textvariable= form_user).grid(row=0,column=1)
 tk.Entry(register_form,textvariable= form_pass,show='*').grid(row=1,column=1)
+
 tk.Button(register_form,text='Register',command=register ).grid(row=2,column=0,columnspan=2,sticky=tk.W+tk.E)
+
 ############################################################################################
 tk.Label(login_form,text= 'Username').grid(row=0,column=0)
 tk.Label(login_form,text= 'Password:').grid(row=1,column=0)
+
 login_user=tk.StringVar()
 login_pass=tk.StringVar()
 tk.Entry(login_form,textvariable=login_user).grid(row=0,column=1)
 tk.Entry(login_form,textvariable=login_pass,show='*').grid(row=1,column=1)
+
 tk.Button(login_form,text='login',command=login).grid(row=2,column=0,columnspan=2,sticky=tk.W+tk.E)
-
-
-
-
 
 
 
