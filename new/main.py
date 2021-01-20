729b33b2b73377f42f765a09f8ee41bbed877d50
awlirezz <68011627+awlirezz@users.noreply.github.com>
2021-01-20 16:56:28 +0330

    hm

--- new/main.py
+++ new/main.py
@@ -1,1 +1,19 @@
+from connection import Connection
+from sqlalchemy.sql.chema import MetaData
+from sqlalchemy.sql import table,insert
 
+
+
+engine = Connection.get_connection()
+session = Connection.creat_session()
+
+
+
+metadata = MetaData(bind=engine)
+student = table('student',metadata,autoload=True)
+
+
+
+i insert(student)
+i = i.values({"name":"alireza","family":"tofighi","grade":99})
+session.excute(i)
