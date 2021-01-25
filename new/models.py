3cc78763718d18e7b5595269e88eddfe079a4451
awlirezz <68011627+awlirezz@users.noreply.github.com>
2021-01-25 17:27:31 +0330

    hm

--- new/models.py
+++ new/models.py
@@ -1,21 +1,28 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, String, Integer, DateTime
+from sqlalchemy import Column, String, Integer, DateTime,TIMESTAMP
 from connection import Connection
+from sqlalchemy.sql.function import func
+
 
 Base = declarative_base()
 
 class Student(Base):
     __tablename__ = 'student'
-    studentId = Column(Integer(), primary_key=True, autoincrement=True)
-    name = Column(String(50))
-    family = Column(String(50))
-    grade = Column(Integer())
-    created_at = Column(DateTime())
+    studentId  = Column(Integer(), primary_key=True, autoincrement=True)
+    first_name = Column(String(225)) 
+    last_name  = Column(String(225))
+    birth_name = Column(Date)
+    grade      = Column(Integer
+    level      = Column(Integer)
+    updated_at = Column(TIMESTAMP,server_default = func.now(),onedate = func.current_timestamp()) 
 
-    def __init__(self, name=None, family=None, grade=None):
-        self.name = name
-        self.family = family
-        self.grade = grade
+
+    def __init__(self, f_n, l_n,b_d , g , l):
+        self.first_name = f_n
+        self.last_name = l_n
+        self.birth_date = b_d
+        self.grade = g
+        self.level = l
 
 engine = Connection().get_connection()
 if not engine.dialect.has_table(engine, 'student'):  # If table don't exist, Create. 
